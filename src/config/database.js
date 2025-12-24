ba610f415f5c83c844ec83c26f132f35818d7e8a
Ayush <ayushsingh24958@gmail.com>
2025-12-24 15:50:02 +0530

    Initial commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://ayushfullstack_db_user:Ayushsingh%40123@cluster0.tqiqhsk.mongodb.net/devTinder"
-  );
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
-
 module.exports = connectDB;
+
